652963a2f1ac8a26a3efe90d04133c7b53f55d5b
Dheeraj Sai <dheerajsaivnr@gmail.com>
2025-08-09 20:52:39 +0530

    Completed basic signup signin content routes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,71 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db.js";
+import { JWT_PASSWORD } from "./config.js"
+import { userMiddleware } from "./middleware.js";
 
 const app =  express();
+app.use(express.json())
 
-app.post("/api/v1/signup", (req,res) => {
+app.post("/api/v1/signup", async (req,res) => {
+    // add zod validation
+    const username = req.body.username;
+    const password = req.body.password;
 
+    try{
+        await UserModel.create({
+            username,
+            password
+        })
+
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e){
+        res.status(411).json({
+            message: "User already exits"
+        })
+    }
 })
 
-app.post("/api/v1/signin", (req,res) => {
+app.post("/api/v1/signin", async (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if(existingUser){
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD);
 
+        res.json({
+            token
+        })
+    }
+    else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 })
 
-app.post("/api/v1/content", (req,res) => {
-
+app.post("/api/v1/content",userMiddleware,async (req,res) => {
+    const link = req.body.link;
+    const title = req.body.title;
+    //@ts-ignore
+    const userId = req.userId;
+    await ContentModel.create({
+        title,
+        link,
+        tags: [],
+        userId
+    })
+    res.json({
+        message: "Content Added"
+    })
 })
 
 app.get("/api/v1/content", (req,res) => {
@@ -29,5 +81,7 @@ app.post("/api/v1/brain/share", (req,res) => {
 })
 
 app.get("/api/v1/brain/:shareLink", (req,res) => {
-    
+
 })
+
+app.listen(3000)
---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import { JWT_PASSWORD } from "./config.js";
+
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const headers = req.headers["authorization"];
+    const decoded = jwt.verify(headers as string,JWT_PASSWORD);
+    console.log(decoded);
+    if(decoded){
+        //@ts-ignore
+        req.userId = decoded.id;
+        next();
+    }else{
+        res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
+}
