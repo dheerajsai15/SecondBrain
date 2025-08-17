19f1f98a8c5321e3093daf279d8db9fb6c199044
Dheeraj Sai <dheerajsaivnr@gmail.com>
2025-08-17 11:28:48 +0530

    Added type to content Schema

--- src/db.ts
+++ src/db.ts
@@ -13,8 +13,16 @@ export const UserModel = model("User",UserSchema);
 const ContentSchema = new Schema({
     title: String,
     link: String,
+    type: String,
     tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
     userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
 })
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true, unique: true}
+});
+
+export const LinkModel = model("Links",LinkSchema);
 
 export const ContentModel = model("Content", ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import { ContentModel, UserModel } from "./db.js";
+import { ContentModel, LinkModel, UserModel } from "./db.js";
 import { JWT_PASSWORD } from "./config.js"
 import { userMiddleware } from "./middleware.js";
+import { random } from "./utils.js";
+import cors from "cors";
 
 const app =  express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.post("/api/v1/signup", async (req,res) => {
     // add zod validation
@@ -55,11 +58,13 @@ app.post("/api/v1/signin", async (req,res) => {
 app.post("/api/v1/content",userMiddleware,async (req,res) => {
     const link = req.body.link;
     const title = req.body.title;
+    const type = req.body.type;
     //@ts-ignore
     const userId = req.userId;
     await ContentModel.create({
         title,
         link,
+        type,
         tags: [],
         userId
     })
@@ -85,8 +90,7 @@ app.delete("/api/v1/content", userMiddleware, async (req,res) => {
     const contentId = req.body.contentId;
 
     await ContentModel.deleteOne({
-        contentId,
-        //@ts-ignore
+        _id: contentId,
         userId: req.userId
     })
 
@@ -95,12 +99,67 @@ app.delete("/api/v1/content", userMiddleware, async (req,res) => {
     })
 })
 
-app.post("/api/v1/brain/share", (req,res) => {
+app.post("/api/v1/brain/share",userMiddleware,async (req,res) => {
+    const share = req.body.share;
+    if(share){
+        const existingLink = await LinkModel.findOne({
+            userId: req.userId
+        })
+        if(existingLink){
+            res.json({
+                message: "/share/" + existingLink.hash,
+                hash: existingLink.hash
+            })
+        }
+        else{
+            const hash = random(10)
+            await LinkModel.create({
+                userId: req.userId,
+                hash: hash
+            })
 
+            res.json({
+                message: "/share/" + hash,
+                hash: hash
+            })
+        }
+    } else{
+        await LinkModel.deleteOne({
+            userId: req.userId
+        })
+
+        res.json({
+            message: "removed link"
+        })
+    }
 })
 
-app.get("/api/v1/brain/:shareLink", (req,res) => {
+app.get("/api/v1/brain/:shareLink",async (req,res) => {
+    const hash = req.params.shareLink;
 
+    const link = await LinkModel.findOne({
+        hash
+    })
+
+    if(!link){
+        res.status(411).json({
+            message: "Sorry Incorrect Input"
+        })
+        return;
+    }
+
+    const content = await ContentModel.find({
+        userId: link.userId
+    })
+
+    const user = await UserModel.findOne({
+        _id: link.userId
+    })
+
+    res.json({
+        username: user?.username,
+        content: content
+    })
 })
 
 app.listen(3000)
